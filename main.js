6a8b8bbb40eee34cf55a8fda607c11ca8c2d5c0f
David Sobecki <sobecki.127@gmail.com>
2020-06-15 21:58:00 -0400

    reservations close btn

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 $(document).ready(function () {
-  // opens navigation
-  $(".nav__toggle-button").click(function () {
+  // Navigation
+  $(".nav__toggle-btn").click(function () {
     $(".nav__slider").toggleClass("active", 500);
     $(".nav__sidebar").toggleClass("active", 500);
     $(".nav__hamburger").toggleClass("active", 500);
-    // $(this).toggleClass("open");
   });
 
+  // Reservations open
   $(".reserve__btn").click(function () {
+    $(".reserve_btn").toggleClass("active");
+    $(".reserve__close-btn").toggleClass("active");
+    $(".reserve__slider").toggleClass("active", 500);
+    $(".reserve__btn").toggleClass("active", 500);
+  });
+
+  $(".reserve__close-btn").click(function () {
+    $(".reserve_btn").toggleClass("active");
+    $(".reserve__close-btn").toggleClass("active");
     $(".reserve__slider").toggleClass("active", 500);
     $(".reserve__btn").toggleClass("active", 500);
   });
