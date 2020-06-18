530a80e98fb902e294e48891ea67560f6676c801
David Sobecki <sobecki.127@gmail.com>
2020-06-18 15:09:25 -0400

    location section

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+  // ================ test ================
+  // https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+  $(document).on("click", 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    //
+    clear(navObj);
+    //
+    setTimeout(() => {
+      $("html, body").animate(
+        {
+          scrollTop: $($.attr(this, "href")).offset().top,
+        },
+        500
+      );
+      //
+    }, 300);
+  });
+  /*
+=========================================
+*/
+  // Clears .active off an obj.element.item
   let clear = (obj) => {
     let { elements } = obj;
     for (let el of elements) {
@@ -6,6 +27,8 @@ $(document).ready(function () {
     }
   };
 
+  // Passed two objects: first to make active, second to take off active
+  // Toggles .active with a delay // prev is passed to clear
   let clickHandler = (current, prev) => {
     let { button, elements, delay } = current;
     button.on("click", () => {
@@ -41,7 +64,6 @@ $(document).ready(function () {
     elements: [
       {
         item: $(".reserve__btn"),
-        text: "Home",
         delay: 200,
       },
       {
@@ -55,6 +77,11 @@ $(document).ready(function () {
     ],
   };
 
+  //
   clickHandler(reserveObj, navObj);
   clickHandler(navObj, reserveObj);
+
+  // fullscreen slider
+  // https://www.jqueryscript.net/animation/touch-fullscreen-scrolling.html
+  // $("#fullpage").fullpage();
 });
